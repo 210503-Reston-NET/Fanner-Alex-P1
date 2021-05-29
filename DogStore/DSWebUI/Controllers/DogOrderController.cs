24c1c46d74e7dc8fbb7eaf9b00ffa7cc57e4d233
afanner <fanner@wisc.edu>
2021-05-29 16:29:41 -0500

    Finally got this drop down working

---
+++ DogStore/DSWebUI/Controllers/DogOrderController.cs
@@ -0,0 +1,108 @@
+﻿using DSBL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSModels;
+using DSWebUI.Models;
+
+namespace DSWebUI.Controllers
+{
+    public class DogOrderController : Controller
+    {
+        private IBuyerBL _buyerBL;
+        private IStoreLocationBL _storeLocationBL;
+        public IOrderBL _orderBL;
+        public DogOrderController(IBuyerBL buyerBL, IStoreLocationBL storeLocationBL, IOrderBL orderBL)
+        {
+            _buyerBL = buyerBL;
+            _storeLocationBL = storeLocationBL;
+            _orderBL = orderBL;
+        }
+        // GET: DogOrderController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DogOrderController/Details/5
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        // GET: DogOrderController/Create
+        public ActionResult Create(int id)
+        {
+            List<DogBuyer> dogBuyers = _buyerBL.GetAllBuyers();
+            List<long> buyerIds = new List<long>();
+            foreach(DogBuyer dB in dogBuyers)
+            {
+                buyerIds.Add(dB.PhoneNumber);
+            }
+            DogOrderVM dogOrderVM = new DogOrderVM();
+            dogOrderVM.BuyerList = buyerIds;
+            dogOrderVM.StoreId = id;
+            return View(dogOrderVM);
+        }
+
+        // POST: DogOrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DogOrderVM dogOrderVM)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogOrderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogOrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogOrderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogOrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DogStore/DSWebUI/Models/DogOrderVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DSWebUI.Models
+{
+    public class DogOrderVM
+    {
+        public int Id { get; set; }
+        public int StoreId { get; set; }
+        public long BuyerId { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        /// <summary>
+        /// Customer ordering the dogs, represented by DogBuyer.
+        /// </summary>
+        /// <value></value>
+        public DogBuyer DogBuyer { get; set; }
+        public List<long> BuyerList { get; set; }
+
+        /// <summary>
+        /// StoreLocation that the customer is ordering from.
+        /// </summary>
+        /// <value></value>
+        public StoreLocation StoreLocation { get; set; }
+        
+        /// <summary>
+        /// Double representing the total of the order.
+        /// </summary>
+        /// <value></value>
+        public double Total { get; set; }
+    }
+}
