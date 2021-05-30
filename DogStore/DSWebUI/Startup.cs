24613757cc3a3d1b9447504cd4d2368803cc855c
afanner <fanner@wisc.edu>
2021-05-29 22:24:45 -0500

    debugging azure stuff

--- DogStore/DSWebUI/Startup.cs
+++ DogStore/DSWebUI/Startup.cs
@@ -51,7 +51,7 @@ namespace DSWebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+           /* if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -60,7 +60,8 @@ namespace DSWebUI
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }*/
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
